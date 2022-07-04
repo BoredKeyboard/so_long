d15e50ffe4356b3a87cbe7f83b21fadf31227c3d
Milan Forsthövel <mforstho@student.codam.nl>
2022-07-04 15:54:59 +0200

    tested some funcitons

--- so_long.c
+++ so_long.c
@@ -6,13 +6,15 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/28 15:54:14 by mforstho      #+#    #+#                 */
-/*   Updated: 2022/06/28 16:45:22 by mforstho      ########   odam.nl         */
+/*   Updated: 2022/07/04 15:53:29 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42/include/MLX42/MLX42.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <memory.h>
 #define WIDTH 256
 #define HEIGHT 256
 
@@ -37,23 +39,23 @@
 // 	return (EXIT_SUCCESS);
 // }
 
-static mlx_image_t	*g_img;
+// static mlx_image_t	*g_img;
 
-int32_t	main(void) // single pixel
-{
-	mlx_t	*mlx;
+// int32_t	main(void) // single pixel
+// {
+// 	mlx_t	*mlx;
 
-	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
-	if (!mlx)
-		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(mlx, 128, 128);
-	mlx_image_to_window(mlx, g_img, 0, 0);
-	mlx_put_pixel(g_img, 64, 64, 0xFFFFFFFF);
-	mlx_loop(mlx);
-	mlx_delete_image(mlx, g_img);
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
-}
+// 	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+// 	if (!mlx)
+// 		exit(EXIT_FAILURE);
+// 	g_img = mlx_new_image(mlx, 128, 128);
+// 	mlx_image_to_window(mlx, g_img, 0, 0);
+// 	mlx_put_pixel(g_img, 64, 64, 0xFFFFFFFF);
+// 	mlx_loop(mlx);
+// 	mlx_delete_image(mlx, g_img);
+// 	mlx_terminate(mlx);
+// 	return (EXIT_SUCCESS);
+// }
 
 // int32_t	main(void) // image
 // {
@@ -68,3 +70,56 @@ int32_t	main(void) // single pixel
 // 	mlx_delete_image(mlx, g_img);
 // 	mlx_terminate(mlx);
 // }
+
+
+
+static mlx_image_t	*g_img;
+
+void	image_hook(mlx_t *mlx)
+{
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_P))
+		mlx_delete_image(mlx, g_img);
+	for (int x = 0; x < g_img->width; x++)
+		for (int y= 0; y < g_img->height; y++)
+			mlx_put_pixel(g_img, x, y, rand() % RAND_MAX);
+}
+
+void	input_hook(mlx_t *mlx)
+{
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		g_img->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		g_img->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		g_img->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		g_img->instances[0].x += 5;
+}
+
+void	hook(void *param)
+{
+	mlx_t	*mlx;
+
+	mlx = param;
+	image_hook(mlx);
+	input_hook(mlx);
+}
+
+int32_t	main(void)
+{
+	mlx_t	*mlx;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!mlx)
+		exit(EXIT_FAILURE);
+	g_img = mlx_new_image(mlx, 256, 256);
+	mlx_image_to_window(mlx, g_img, 0, 0);
+	mlx_loop_hook(mlx, &hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
