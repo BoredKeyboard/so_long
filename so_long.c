c4861f8ef9ed40f2099c11cdd631962459684810
Milan Forsthövel <mforstho@student.codam.nl>
2022-07-04 16:27:38 +0200

    Image/texture and movement testing

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/28 15:54:14 by mforstho      #+#    #+#                 */
-/*   Updated: 2022/07/04 15:53:29 by mforstho      ########   odam.nl         */
+/*   Updated: 2022/07/04 16:27:06 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,54 +71,68 @@
 // 	mlx_terminate(mlx);
 // }
 
+typedef struct	s_data
+{
+	mlx_t		*mlx;
+	mlx_image_t	*image;
+}	t_data;
 
-
-static mlx_image_t	*g_img;
-
-void	image_hook(mlx_t *mlx)
+void	image_hook(mlx_t *mlx, mlx_image_t *image)
 {
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_P))
-		mlx_delete_image(mlx, g_img);
-	for (int x = 0; x < g_img->width; x++)
-		for (int y= 0; y < g_img->height; y++)
-			mlx_put_pixel(g_img, x, y, rand() % RAND_MAX);
+		mlx_delete_image(mlx, image);
+	// for (int x = 0; x < image->width; x++)
+	// 	for (int y= 0; y < image->height; y++)
+	// 		mlx_put_pixel(image, x, y, rand() % RAND_MAX);
 }
 
-void	input_hook(mlx_t *mlx)
+void	input_hook(mlx_t *mlx, mlx_image_t *image)
 {
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		g_img->instances[0].y -= 5;
+		image->instances[0].y -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		g_img->instances[0].y += 5;
+		image->instances[0].y += 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		g_img->instances[0].x -= 5;
+		image->instances[0].x -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		g_img->instances[0].x += 5;
+		image->instances[0].x += 5;
 }
 
 void	hook(void *param)
 {
-	mlx_t	*mlx;
+	t_data		*data;
+	mlx_t		*mlx;
+	mlx_image_t	*image;
 
-	mlx = param;
-	image_hook(mlx);
-	input_hook(mlx);
+	data = param;
+	mlx = data->mlx;
+	image = data->image;
+	image_hook(mlx, image);
+	input_hook(mlx, image);
 }
 
 int32_t	main(void)
 {
-	mlx_t	*mlx;
+	mlx_t			*mlx;
+	mlx_texture_t	*texture;
+	mlx_image_t		*image;
+	const uint32_t	xy[2] = {0, 0};
+	const uint32_t	wh[2] = {256, 256};
+	t_data			data;
 
 	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(mlx, 256, 256);
-	mlx_image_to_window(mlx, g_img, 0, 0);
-	mlx_loop_hook(mlx, &hook, mlx);
+	texture = mlx_load_png("grass.png");
+	image = mlx_texture_area_to_image(mlx, texture, xy, wh);
+	mlx_image_to_window(mlx, image, 0, 0);
+	data.mlx = mlx;
+	data.image = image;
+	mlx_loop_hook(mlx, &hook, &data);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
