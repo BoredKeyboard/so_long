92e16cd6a3291e093fa927e4c03c5c5906a1b247
Milan Forsthövel <mforstho@student.codam.nl>
2022-07-07 16:32:25 +0200

    Image stuff

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/28 15:54:14 by mforstho      #+#    #+#                 */
-/*   Updated: 2022/07/05 15:59:59 by mforstho      ########   odam.nl         */
+/*   Updated: 2022/07/07 16:12:18 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@
 
 typedef struct s_player
 {
-	int			x;
-	int			y;
-	int			health;
+	mlx_instance_t	*instance;
+	int				health;
 }	t_player;
 
-typedef struct	s_data
+typedef struct s_data
 {
 	mlx_t		*mlx;
 	mlx_image_t	*image;
@@ -40,18 +39,36 @@ void	image_hook(mlx_t *mlx, mlx_image_t *image)
 		mlx_delete_image(mlx, image);
 }
 
-void	input_hook(mlx_t *mlx, mlx_image_t *image)
+// void	input_hook(mlx_t *mlx, mlx_image_t *image)
+// {
+// 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+// 		mlx_close_window(mlx);
+// 	if (mlx_is_key_down(mlx, MLX_KEY_W))
+// 		image->instances[0].y -= 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_S))
+// 		image->instances[0].y += 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_A))
+// 		image->instances[0].x -= 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_D))
+// 		image->instances[0].x += 5;
+// }
+
+void	input_hook(t_data *data)
 {
+	const t_player	*player = &data->player;
+	mlx_t			*mlx;
+
+	mlx = data->mlx;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_W))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_S))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_A))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_D))
-		image->instances[0].x += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_W) || mlx_is_key_down(mlx, MLX_KEY_UP))
+		player->instance->y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_S) || mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		player->instance->y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_A) || mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		player->instance->x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_D) || mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		player->instance->x += 5;
 }
 
 void	hook(void *param)
@@ -64,30 +81,30 @@ void	hook(void *param)
 	mlx = data->mlx;
 	image = data->image;
 	image_hook(mlx, image);
-	input_hook(mlx, image);
+	input_hook(data);
 }
 
 int32_t	main(void)
 {
 	mlx_t			*mlx;
 	mlx_texture_t	*texture;
-	mlx_image_t		*image;
 	uint32_t		xy[2];
 	uint32_t		wh[2];
 	t_data			data;
 
 	xy[0] = 16 * 5 * 8;
-	xy[1] = 16 * 15 * 5 - 8;
+	xy[1] = 16 * 15 * 5;
 	wh[0] = 16 * 5;
 	wh[1] = 16 * 5;
 	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
 	texture = mlx_load_png("dungeonTileset.png");
-	image = mlx_texture_area_to_image(mlx, texture, xy, wh);
-	mlx_image_to_window(mlx, image, 0, 0);
+	data.image = mlx_texture_area_to_image(mlx, texture, xy, wh);
+	int32_t i = mlx_image_to_window(mlx, data.image, 80, 80);
+	// data.image->instances[i];
+	data.player.instance = &data.image->instances[i];
 	data.mlx = mlx;
-	data.image = image;
 	mlx_loop_hook(mlx, &hook, &data);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
